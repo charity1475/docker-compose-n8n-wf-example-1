671abbfc1b2006c038eea4c8d769608869f4c544
Charity <charitymbisi@gmail.com>
2023-12-06 15:33:01 +0300

    feat: Added a body parser middleware

--- n8n-application-post/simple-api-rest/server.js
+++ n8n-application-post/simple-api-rest/server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser');
 
 // Start the server on port 3000
 app.listen(3000, () => {
   console.log('API server listening on port 3000');
 });
 
+// Body parser middleware
+app.use(bodyParser.json());
+
 // Define a route for the GET method at the root path
 app.get('/', (req, res) => {
-    res.send('simple api!');
-  });
+  res.send('simple api!');
+});
 
-app.post('/products/mysql', function(req, res) {
-  res.json({ status: 'success' })   
+app.post('/products/mysql', function (req, res) {
+  console.log(`Received item: ${JSON.stringify(req.body)}`)
+  res.json({ status: 'success' })
 })
